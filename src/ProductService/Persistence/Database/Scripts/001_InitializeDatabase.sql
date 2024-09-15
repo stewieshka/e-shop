84bc42d35ca1f4a894568801268637c6cb53c4ae
Stepan Golub <stewieshka@icloud.com>
2024-09-15 22:35:03 +0300

    Update product service

--- src/ProductService/Persistence/Database/Scripts/001_InitializeDatabase.sql
+++ src/ProductService/Persistence/Database/Scripts/001_InitializeDatabase.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE 
-IF NOT EXISTS products (
-    id UUID PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    category TEXT NOT NULL,
-    price double precision NOT NULL,
-    stock int NOT NULL
+﻿CREATE TABLE
+    IF NOT EXISTS products
+(
+    id          UUID PRIMARY KEY,
+    name        TEXT             NOT NULL,
+    description TEXT             NOT NULL,
+    category    TEXT             NOT NULL,
+    price       double precision NOT NULL,
+    stock       int              NOT NULL
 );
